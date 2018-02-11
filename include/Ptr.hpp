846f188c60c8c0a30a8765554bf5eea4cd6ee3fa
Max Vu <maxwell.vu@gmail.com>
2018-02-10 20:39:16 -0500

    Steal Ptr, too

---
+++ include/Ptr.hpp
@@ -0,0 +1,100 @@
+#ifndef RILL_PTR
+#define RILL_PTR
+
+namespace Rill {
+
+    namespace {
+
+        template <typename T> struct PtrSlot {
+
+            T data;
+            size_t count;
+
+            PtrSlot () : count( 0 ) {
+
+            }
+
+            PtrSlot ( const T & init ) : data( init ), count( 0 ) {
+
+            }
+
+        };
+
+    };
+
+    template <typename T> class Ptr {
+
+        protected:
+
+        PtrSlot<T> * slot;
+
+        bool isUnique () const {
+            return this->slot->count == 1;
+        }
+
+        Ptr<T> & makeUnique () {
+            if ( this->isUnique() )
+                return *this;
+            this->slot->count--;
+            this->slot = new PtrSlot<T>( this->slot->data );
+            this->slot->count++;
+            return *this;
+        }
+
+        public:
+
+        Ptr () : slot( new PtrSlot<T> ) {
+            this->slot->count++;
+        }
+
+        Ptr ( const T & init ) : slot( new PtrSlot<T>( init ) ) {
+            this->slot->count++;
+        }
+
+        Ptr ( const Ptr<T> & other ) : slot( other.slot ) {
+            this->slot->count++;
+        }
+
+        ~Ptr () {
+            if ( !--this->slot->count )
+                delete this->slot;
+        }
+
+        Ptr & operator= ( const Ptr<T> & other ) {
+            if ( !--this->slot->count )
+                delete this->slot;
+            this->slot = other.slot;
+            this->slot->count++;
+        }
+
+        bool operator== ( const Ptr<T> & other ) const {
+            return this->slot == other.slot;
+        }
+
+        bool operator!= ( const Ptr<T> & other ) const {
+            return !( *this == other );
+        }
+
+        const T & operator* () const {
+            return this->slot->data;
+        }
+
+        const T * operator-> () const {
+            return this->slot->data;
+        }
+
+        T & operator* () {
+            this->makeUnique();
+            return this->slot->data;
+        }
+
+        T * operator-> () {
+            this->makeUnique();
+            return this->slot->data;
+        }
+
+    };
+
+};
+
+#endif

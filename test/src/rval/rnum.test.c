e31adf986ee59b857efa7cfcc111839a69fd3d34
Max Vu <maxwell.vu@gmail.com>
2018-12-04 22:18:27 -0500

    Start over.

--- test/src/rval/rnum.test.c
+++
@@ -1,3 +0,0 @@
-void test_rval_rnum () {
-
-}
--- test/src/rval/rval.test.c
+++
@@ -1,8 +0,0 @@
-#include "rval.test.h"
-
-void test_rval () {
-    test_rval_rnum();
-    test_rval_rbuf();
-    test_rval_rvec();
-    test_rval_rmap();
-}

67717b32e55209f99e1e311d693e3f5b6d01e534
Max Vu <maxwell.vu@gmail.com>
2018-12-13 23:03:21 -0500

    Add some VM stuff.

--- include/vm/rvm.h
+++ include/vm/rvm.h
@@ -1,7 +1,12 @@
 typedef struct rvm {
-    RVector * ds;
-    RVector * ss;
-    RBuffer * iq;
-    size_t    pc;
-    RVector * ct;
+    RVector * ds; // data stack
+    RVector * ss; // side stack
+    RVector * cs; // call stack
+
+    RBuffer * it; // instruction table
+    size_t    pc; // program counter
+    RVector * ct; // constant table
 } RVM;
+
+RVM * rvm_create ();
+void rvm_destroy ();
